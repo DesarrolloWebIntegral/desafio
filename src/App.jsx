51eee10a22f269e5251c3ce6e55ffcef92fff3a8
gbarron2014 <gbarron@utng.edu.mx>
2026-07-02 15:59:23 -0600

    Modificando en FrontEnd

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
 
 function App() {
-  
-
   return (
     <>
-      <section> 
-        <h1> Update FrontEnd </h1>
+      <section>
+        <h1> Update FrontEnd one more time </h1>
       </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
